9fd2f2fe6a4fe65d7d56b45d96f8488ff34f025d
Bumbilo <dimagolyak@gmail.com>
2017-12-18 15:26:45 +0200

    create self directive

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,13 +9,37 @@ import {ProductRepository} from '../model/product.repository';
 })
 export class StoreComponent implements OnInit {
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private _repository: ProductRepository) {
   }
 
   get products(): Product[] {
-    return this._repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this._repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this._repository
+      .getProducts(this.selectedCategory).length / this.productsPerPage)
+  }
+
+  // get pageNumbers(): number[] {
+  //   return Array(Math.ceil(this._repository
+  //     .getProducts(this.selectedCategory).length / this.productsPerPage))
+  //     .fill(0).map((x, i) => i + 1);
+  // }
 
   get categories(): string[] {
     return this._repository.getCategories();
