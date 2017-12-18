505f1810348ac58b358e88e3daf56a230cd6e9cc
Bumbilo <dimagolyak@gmail.com>
2017-12-18 14:43:20 +0200

    thing filter

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../model/product.model';
+import {ProductRepository} from '../model/product.repository';
 
 @Component({
   selector: 'app-store',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
+  public selectedCategory = null;
 
-  constructor() { }
+  constructor(private _repository: ProductRepository) {
+  }
+
+  get products(): Product[] {
+    return this._repository.getProducts(this.selectedCategory);
+  }
+
+  get categories(): string[] {
+    return this._repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
 
   ngOnInit() {
   }
