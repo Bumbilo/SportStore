957d11793a6290f3df1a8d86d79e775a2fbc977c
Bumbilo <dimagolyak@gmail.com>
2017-12-22 15:46:24 +0200

    added admin features

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth. component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth. guard';
-import { ProductTableComponent } from './productTable.component';
+import { ProductTableComponent } from './productTable.component'
 import { ProductEditorComponent } from './productEditor.component';
 import { OrderTableComponent } from './orderTable.component';
 
